f199eb5285c9ce89bf3455b209e7cb011931ba5b
jenazzad <mansaryouness@gmail.com>
2020-03-27 08:25:24 +0100

    full demo

--- app.py
+++ app.py
@@ -8,6 +8,20 @@ import plotly.express as px
 from models import get_model_mlp, get_model_cnn
 
 
+colors = [
+    "#1f77b4",  # muted blue
+    "#ff7f0e",  # safety orange
+    "#2ca02c",  # cooked asparagus green
+    "#d62728",  # brick red
+    "#9467bd",  # muted purple
+    "#8c564b",  # chestnut brown
+    "#e377c2",  # raspberry yogurt pink
+    "#7f7f7f",  # middle gray
+    "#bcbd22",  # curry yellow-green
+    "#17becf",  # blue-teal
+]
+
+
 @st.cache
 def get_data(flatten=True):
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -15,9 +29,12 @@ def get_data(flatten=True):
     if flatten:
         x_train = x_train.reshape(60000, 784)
         x_test = x_test.reshape(10000, 784)
+    else:
+        x_train = x_train[..., np.newaxis]
+        x_test = x_test[..., np.newaxis]
 
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
+    x_train = x_train.astype("float32")
+    x_test = x_test.astype("float32")
     x_train /= 255
     x_test /= 255
 
@@ -29,41 +46,63 @@ def get_data(flatten=True):
 
 progress_bar = st.progress(0)
 status_text = st.empty()
-plotly_chart = st.empty()
+plotly_chart_1 = st.empty()
+plotly_chart_2 = st.empty()
 
 
 option = st.sidebar.selectbox(
-    'Which architecture do you want to use ?',
-    ("None", "MLP", "CNN"))
+    "Which architecture do you want to use ?", ("None", "MLP", "CNN")
+)
 
 x_train, y_train, x_test, y_test = get_data(flatten=(option == "MLP"))
 
 if option == "MLP":
-    model = get_model_mlp()
+    model, model_aux = get_model_mlp()
 else:
-    model = get_model_cnn()
+    model, model_aux = get_model_cnn()
 
 if option in ("MLP", "CNN"):
 
     accuracies = []
 
     for i in range(20):
-        progress_bar.progress(5 * (i+1))
+        progress_bar.progress(5 * (i + 1))
 
-        history = model.fit(x_train, y_train, validation_data=(x_test, y_test), nb_epoch=1)
+        history = model.fit(
+            x_train, y_train, validation_data=(x_test, y_test), nb_epoch=1
+        )
 
-        acc = float(history.history['val_acc'][0])
+        acc = float(history.history["val_acc"][0])
         accuracies.append(acc)
 
-        fig = px.line(x=list(range(len(accuracies))), y=accuracies, title="Val accuracy")
-        fig.update_xaxes(range=[-1, 21])
-        fig.update_yaxes(range=[accuracies[0]-0.1, 1])
+        fig_1 = px.line(
+            x=list(range(len(accuracies))),
+            y=accuracies,
+            title="Validation Accuracy vs Epoch",
+            labels={"x": "Epoch", "y": "Accuracy"},
+        )
+        fig_1.update_xaxes(range=[-1, 21])
+        fig_1.update_yaxes(range=[accuracies[0] - 0.1, 1])
 
-        plotly_chart.plotly_chart(fig)
+        plotly_chart_1.plotly_chart(fig_1)
 
+        test_repr = model_aux.predict(x_test)
+        test_label = np.argmax(y_test, axis=-1)
+
+        fig_2 = px.scatter(
+            x=test_repr[:, 0],
+            y=test_repr[:, 1],
+            title="Test Sample representation",
+            labels={"x": "Dim 1", "y": "Dim 2"},
+            color=[
+                str(a) for a in test_label.tolist()
+            ],  # [colors[a] for a in test_label.tolist()]
+        )
+
+        plotly_chart_2.plotly_chart(fig_2)
 
         time.sleep(0.1)
 
-    status_text.text('Done!')
+    status_text.text("Done!")
 
     st.balloons()
--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from tensorflow.keras.layers import (
     Input,
     GlobalMaxPooling2D,
     Convolution2D,
-    MaxPooling2D
+    MaxPooling2D,
 )
 from tensorflow.keras.losses import categorical_crossentropy
 from tensorflow.keras.models import Model
@@ -11,8 +11,7 @@ from tensorflow.keras.optimizers import Adam
 
 
 def get_model_cnn(
-        input_shape=(None, None, 1),
-        n_classes=10,
+    input_shape=(None, None, 1), n_classes=10,
 ):
     inputs = Input(input_shape)
 
@@ -27,29 +26,33 @@ def get_model_cnn(
     x = GlobalMaxPooling2D()(x)
 
     out1 = Dense(2, activation="linear")(x)
-    out = Dense(50, activation="relu")(out1)
+    out = Dense(10, activation="relu")(out1)
     out = Dense(n_classes, activation="softmax")(out)
 
     model = Model(inputs, out)
-    model.compile(optimizer=Adam(0.0001), loss=categorical_crossentropy, metrics=["acc"])
+    model.compile(
+        optimizer=Adam(0.0001), loss=categorical_crossentropy, metrics=["acc"]
+    )
 
     model_aux = Model(inputs, out1)
-    model_aux.compile(optimizer=Adam(0.0001), loss=categorical_crossentropy, metrics=["acc"])
+    model_aux.compile(
+        optimizer=Adam(0.0001), loss=categorical_crossentropy, metrics=["acc"]
+    )
 
     model.summary()
 
-    return model
+    return model, model_aux
 
 
 def get_model_mlp(
-        input_shape=(784,), n_classes=10,
+    input_shape=(784,), n_classes=10,
 ):
     inputs = Input(input_shape)
     x = Dense(32, activation="relu")(inputs)
     x = Dense(32, activation="relu")(x)
 
     out1 = Dense(2, activation="linear")(x)
-    out = Dense(50, activation="relu")(out1)
+    out = Dense(10, activation="relu")(out1)
     out = Dense(n_classes, activation="softmax")(out)
 
     model = Model(inputs, out)
@@ -59,8 +62,10 @@ def get_model_mlp(
     )
 
     model_aux = Model(inputs, out1)
-    model_aux.compile(optimizer=Adam(0.0001), loss=categorical_crossentropy, metrics=["acc"])
+    model_aux.compile(
+        optimizer=Adam(0.0001), loss=categorical_crossentropy, metrics=["acc"]
+    )
 
     model.summary()
 
-    return model
+    return model, model_aux
